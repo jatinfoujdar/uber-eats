d80437b660fa25c9d988005c3c6c121a65690180
jatin foujdar <jatin321001@gmail.com>
2022-10-28 00:32:21 +0530

    second commit

---
+++ src/components/HeadlineCards.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const HeadlineCards = () => {
+  return (
+    <div className='max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6'>
+        <div className="rounded-xl relative">
+            <div className='absolute w-full h-full bg-black/50 rounded-xl text-white '>
+                <p className="font-bold text-2xl px-2 pt-4">Sun's Out , Bogo's Out</p>
+                <p className='px-2'>Through 24/7</p>
+                <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+            </div>
+            <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src='https://images.pexels.com/photos/718742/pexels-photo-718742.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt=''></img>
+        </div>
+        <div className="rounded-xl relative">
+            <div className='absolute w-full h-full bg-black/50 rounded-xl text-white '>
+                <p className="font-bold text-2xl px-2 pt-4">New Resturants</p>
+                <p className='px-2'>Added Daily</p>
+                <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+            </div>
+            <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src='https://images.pexels.com/photos/370984/pexels-photo-370984.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt=''></img>
+        </div>
+        <div className="rounded-xl relative">
+            <div className='absolute w-full h-full bg-black/50 rounded-xl text-white '>
+                <p className="font-bold text-2xl px-2 pt-4">We Deliver Desserts Too </p>
+                <p className='px-2'>Tasty Treats</p>
+                <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+            </div>
+            <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src='https://images.pexels.com/photos/3026810/pexels-photo-3026810.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt=''></img>
+        </div>
+    </div>
+  )
+}
+
+export default HeadlineCards
